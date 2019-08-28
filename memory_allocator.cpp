60be78464a7f9c2de717a01194c0e3cfdaee4ffe
Matthew Eletva <s3716853@student.rmit.edu.au>
2019-08-28 15:57:52 +1000

    Implemented command line arguments to select managing type

--- memory_allocator.cpp
+++ memory_allocator.cpp
@@ -1,25 +1,54 @@
 #include <iostream>
+#include <sys/resource.h>
 #include <unistd.h>
 #include "memory_manager.h"
 
 
-void test(MemoryManager * manager);
+void test(MemoryManager * manager, std::string typeName);
+void testAll(MemoryManager * manager);
 void runAllTests(MemoryManager * manager);
 void testMethod1(MemoryManager * manager);
 void testMethod2(MemoryManager * manager);
 void testMethod3(MemoryManager * manager);
-void testMethod4(MemoryManager * manager);
+//void testMethod4(MemoryManager * manager);
 
-int main(int argc, char *argv[]){
-    
+int main(int argc, char ** argv){
     MemoryManager * manager = new MemoryManager();
-    test(manager);
+    if(argc > 1) {
+        std::string arg = argv[1];
+        if(arg == "-f"){
+            manager->setFirstFit();
+            test(manager, "FIRST");
+        }else if(arg == "-w"){
+            manager->setWorstFit();
+            test(manager, "WORST");
+        }else if (arg == "-b"){
+            manager->setBestFit();
+            test(manager, "BEST");
+        }else{
+            std::cout << "Invalid memory managing type" << std::endl <<
+            "{executable} -{type}" << std::endl <<
+            "{type} = f/b/w/a (first/best/worst/all)" << std::endl;   
+        }
+    }else{
+        testAll(manager);
+    }
 }
 
-void test(MemoryManager * manager){
+void test(MemoryManager * manager, std::string typeName){
+    std::cout << "----------" << typeName << "----------" << std::endl;
+    runAllTests(manager);
+
+    // struct rusage r_usage;
+    // getrusage(RUSAGE_SELF, &r_usage);
+    // std::cout << r_usage.ru_maxrss << std::endl;
+}
+
+void testAll(MemoryManager * manager){
     std::cout << "----------FIRST FIT----------" << std::endl;
     manager->setFirstFit();
     runAllTests(manager);
+
 
     std::cout << std::endl << std::endl << "----------BEST FIT----------" << std::endl;
     manager->setBestFit();
@@ -34,7 +63,7 @@ void runAllTests(MemoryManager * manager){
     testMethod1(manager);
     testMethod2(manager);
     testMethod3(manager);
-    testMethod4(manager);
+    //testMethod4(manager);
 }
 
 //tests creating a large memory location, small one, then deleleting both
@@ -88,19 +117,19 @@ void testMethod3(MemoryManager * manager){
 }
 
 //
-void testMethod4(MemoryManager * manager){
-    manager->reset();
-    std::cout << std::endl << "||TEST METHOD 4||" << std::endl;
-    void * test = manager->alloc(40);
-    void * test2 = manager->alloc(sizeof(int));
-    std::cout << test << std::endl;
-    std::cout << test2 << std::endl;
-    manager->dealloc(test);
-    manager->dealloc(test2);
-    char* num = (char*)manager->alloc(sizeof(int));
-    char* num2 = (char*)manager->alloc(sizeof(int));
-    char* num3 = (char*)manager->alloc(20);
-    std::cout << (void*)num << std::endl;
-    std::cout << (void*)num2 << std::endl;
-    std::cout << (void*)num3 << std::endl;
-}
+// void testMethod4(MemoryManager * manager){
+//     manager->reset();
+//     std::cout << std::endl << "||TEST METHOD 4||" << std::endl;
+//     void * test = manager->alloc(40);
+//     void * test2 = manager->alloc(sizeof(int));
+//     std::cout << test << std::endl;
+//     std::cout << test2 << std::endl;
+//     manager->dealloc(test);
+//     manager->dealloc(test2);
+//     char* num = (char*)manager->alloc(sizeof(int));
+//     char* num2 = (char*)manager->alloc(sizeof(int));
+//     char* num3 = (char*)manager->alloc(20);
+//     std::cout << (void*)num << std::endl;
+//     std::cout << (void*)num2 << std::endl;
+//     std::cout << (void*)num3 << std::endl;
+// }
